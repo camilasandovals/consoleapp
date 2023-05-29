6224bdd974664d7208213f7c06ac07fe4cf2a88f
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-29 00:08:01 -0400

    added humanizer package

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
 Console.WriteLine("Please enter your name");
 var name = Console.ReadLine();
 Console.WriteLine("Please enter your role");
 var role = Console.ReadLine();
 Console.WriteLine("Please enter your age");
-var age = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"My name is {name}, mi role is {role}, and I am {age} years old");
+Console.WriteLine($"My name is {name}, my role is {role}, and I am {age.ToWords()} years old");
+Console.WriteLine($"Mi nombre es {name}, mi role es {role}, y tengo {age.ToWords(new System.Globalization.CultureInfo("es"))} anios");
