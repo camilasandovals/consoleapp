a89355fb2199f607f51c24fad208cff04eefa067
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-28 23:36:07 -0400

    added basic code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please enter your name");
+var name = Console.ReadLine();
+Console.WriteLine("Please enter your role");
+var role = Console.ReadLine();
+Console.WriteLine("Please enter your age");
+var age = Console.ReadLine();
+
+Console.WriteLine($"My name is {name}, mi role is {role}, and I am {age} years old");
